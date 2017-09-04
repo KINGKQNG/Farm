ddf6a271623122ac85c173deef10d75b54275cd7
KINGKQNG <31554087+KINGKQNG@users.noreply.github.com>
2017-09-04 15:52:02 -0400

    Add files via upload

---
+++ Farm/opencloseandswapinstance.sikuli/opencloseandswapinstance.py
@@ -0,0 +1,25 @@
+find("1504483917427.png")
+doubleClick("1504484616138.png")
+wait(45)
+hover("1504484694550.png")
+click("1504484723086.png")
+wait(5)
+find("1504483973418.png")
+click("1504483983973.png")
+wait(40)
+hover(Pattern("1504554138017.png").targetOffset(85,-15))
+wait(3)
+click(Pattern("1504484122807.png").targetOffset(77,-14))
+wait(5)
+find("1504484180304.png")
+click("1504484188184.png")
+click("1504484372601.png")
+wait (45)
+click("1504484437175.png")
+
+
+
+
+
+
+

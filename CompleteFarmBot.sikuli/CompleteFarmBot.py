d286500d5483fbb898a729866dffc085d1a4ded6
KINGKQNG <31554087+KINGKQNG@users.noreply.github.com>
2017-09-06 19:56:30 -0400

    Add files via upload

---
+++ CompleteFarmBot.sikuli/CompleteFarmBot.py
@@ -0,0 +1,105 @@
+for step in range (1):
+    find("1504652955092.png")
+    doubleClick("1504652966755.png")
+    wait (5)
+    find("1504653073706.png")
+    click(Pattern("1504653091776.png").targetOffset(165,-116))
+    wait (50)
+    find("1504653725536.png")
+    click("1504653734936.png")
+    wait (60)
+    if exists("1504739872138.png"):
+        click("1504739824391.png")
+    wait (3)
+    find("1504672715777.png")
+    click("1504672731293.png")      
+    click("1504672782236.png")
+    find("1504673299307.png")    
+    click(Pattern("1504673312975.png").targetOffset(361,7))        
+    click("1504740362241.png")
+    for step in range (2):
+        click("1504672837055.png")        
+        click("1504740540356.png")
+        find("1504654633557.png")
+        click(Pattern("1504654649617.png").targetOffset(160,-27))
+        for step in range (6):
+            click(Pattern("1504654692036.png").similar(0.85))
+        click(Pattern("1504654747015.png").similar(0.74))
+        click("1504654764931.png")
+        wait(1)           
+        click("1504654779038.png")
+        wait (60)
+        for step in range (2):
+            click("1504673092736.png")
+            click("1504740550765.png")            
+            find("1504667264956.png")
+            click(Pattern("1504667284533.png").targetOffset(166,-30))
+            for step in range (6):
+                click("1504667319549.png")
+          
+            click("1504667372927.png")
+            click("1504667387522.png")
+            wait(1)
+            click("1504667415784.png")
+            wait (60)
+        for step in rage (2):
+            click("1504673172379.png")            
+            click("1504740562390.png")            
+            find("1504667538798.png")
+            click(Pattern("1504667545657.png").targetOffset(161,-29))
+            for step in range (6):
+                click("1504667569353.png")
+            click("1504667575260.png")
+            click("1504667763532.png")
+            wait (1)
+            click("1504667797192.png")
+            wait(60)
+        for step in range (2):
+            click("1504673195230.png")
+            click("1504740576661.png")            
+            find("1504667874084.png")
+            click(Pattern("1504667883965.png").targetOffset(144,-25))
+            for step in range (6):
+                click("1504667909688.png")
+            click("1504668184112.png")
+            click("1504668196655.png")
+            wait (1)
+            click("1504668219841.png")
+            wait(60)
+        for step in range (2):
+            click("1504673247722.png")            
+            click("1504740588214.png")            
+            find("1504668479335.png")
+            click(Pattern("1504668456534.png").targetOffset(163,-17))
+            for step in range (6):
+                click("1504668353985.png")
+            click("1504668372346.png")
+            click("1504668385000.png")
+            wait (1)
+            click("1504668404565.png")
+            wait(20)
+        find("1504668521470.png")
+        click(Pattern("1504668533533.png").targetOffset(86,0))
+        find("1504668618487.png")
+        hover("1504668644875.png")
+        click("1504668653259.png")
+        find("1504668701261.png")
+        click(Pattern("1504668689123.png").targetOffset(174,-45))
+        
+        
+        
+            
+            
+                
+
+            
+            
+            
+            
+            
+            
+            
+        
+        
+
+        
